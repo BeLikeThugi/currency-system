4c99548addb4f0ed9ea6b75fba09d298f7a11483
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2021-12-17 23:59:32 +0500

    DOCS UPDATE FINALLY

--- examples/custom/index.md
+++
@@ -1,5 +0,0 @@
-# Custom Commands
-Click them to view more info about the methods.
-
-1. Use Command to use User items & earn money.
-[`use`](https://bintelligent.github.io/currency-system/examples/custom/use)
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2E2D2D',
+      }
+    },
+    fontFamily: {
+      text: ['Passion One'],
+      Staatliches: ['Staatliches']
+    }
+  },
+  variants: {
+    extend: {
+      animation: ['motion-safe'],
+    },
+  },
+  plugins: [],
+}
